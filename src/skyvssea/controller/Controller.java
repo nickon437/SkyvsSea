39c0392e4a6aed68aaa6d666cb3cd6916021a189
rmit-s3762629-ton-binh-on <48907239+rmit-s3762629-ton-binh-on@users.noreply.github.com>
2020-04-15 21:25:16 +1000

    Fixed ghost piece after moved bug

--- src/skyvssea/controller/Controller.java
+++ src/skyvssea/controller/Controller.java
@@ -18,38 +18,46 @@ public class Controller {
             board.clearHighlightedTiles();
             tile.setPiece(pieceManager.getCurrentPiece());
             board.getCurrentTile().removePiece();
-        } else if (tile.hasPiece()) {
+
+            System.out.println("Move " + pieceManager.getCurrentPiece().getName() + " from " +
+                    board.getCurrentTile().getTilePane().getX() + " " +
+                    board.getCurrentTile().getTilePane().getY() + " to " +
+                    tile.getTilePane().getX() + " " +
+                    tile.getTilePane().getY());
+        } else {
             board.clearHighlightedTiles();
-            // Nick - TODO: Check whose the piece belongs to
-            Piece piece = tile.getPiece();
-            pieceManager.setCurrentPiece(piece);
+            if (tile.hasPiece()) {
+                // Nick - TODO: Check whose the piece belongs to
+                Piece piece = tile.getPiece();
+                pieceManager.setCurrentPiece(piece);
 
-            int numMove = piece.getNumMove();
-            int pieceX = tile.getTilePane().getX();
-            int pieceY = tile.getTilePane().getY();
-            Tile[][] tiles = board.getTiles();
+                int numMove = piece.getNumMove();
+                int pieceX = tile.getTilePane().getX();
+                int pieceY = tile.getTilePane().getY();
+                Tile[][] tiles = board.getTiles();
 
-            // Nick - TODO: Find a way to modularize the code
-            // Highlight possible move tiles
-            for (int count = 1; count <= numMove; count++) {
-                if ((pieceX + count) < BoardPane.NUM_SIDE_CELL) {
-                    Tile rightTile = tiles[pieceX + count][pieceY];
-                    board.highlightUnoccupiedTiles(rightTile);
-                }
+                // Nick - TODO: Find a way to modularize the code
+                // Highlight possible move tiles
+                for (int count = 1; count <= numMove; count++) {
+                    if ((pieceX + count) < BoardPane.NUM_SIDE_CELL) {
+                        Tile rightTile = tiles[pieceX + count][pieceY];
+                        board.highlightUnoccupiedTiles(rightTile);
+                    }
 
-                if ((pieceX - count) >= 0) {
-                    Tile leftTile = tiles[pieceX - count][pieceY];
-                    board.highlightUnoccupiedTiles(leftTile);
-                }
+                    if ((pieceX - count) >= 0) {
+                        Tile leftTile = tiles[pieceX - count][pieceY];
+                        board.highlightUnoccupiedTiles(leftTile);
+                    }
 
-                if ((pieceY + count) < BoardPane.NUM_SIDE_CELL) {
-                    Tile downTile = tiles[pieceX][pieceY + count];
-                    board.highlightUnoccupiedTiles(downTile);
-                }
+                    if ((pieceY + count) < BoardPane.NUM_SIDE_CELL) {
+                        Tile downTile = tiles[pieceX][pieceY + count];
+                        board.highlightUnoccupiedTiles(downTile);
+                    }
 
-                if ((pieceY - count) >= 0) {
-                    Tile upTile = tiles[pieceX][pieceY - count];
-                    board.highlightUnoccupiedTiles(upTile);
+                    if ((pieceY - count) >= 0) {
+                        Tile upTile = tiles[pieceX][pieceY - count];
+                        board.highlightUnoccupiedTiles(upTile);
+                    }
                 }
             }
         }

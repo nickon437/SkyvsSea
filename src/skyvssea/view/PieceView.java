0b1be4e65d3a2af581c42254b51557e6c81cefe4
rmit-s3762629-ton-binh-on <48907239+rmit-s3762629-ton-binh-on@users.noreply.github.com>
2020-04-15 21:54:45 +1000

    Changed Label to Text in PieceView

--- src/skyvssea/view/PieceView.java
+++ src/skyvssea/view/PieceView.java
@@ -1,34 +1,32 @@
 package skyvssea.view;
 
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+
 
 public class PieceView extends StackPane {
     private Circle circle;
-    private Label label;
+    private Text text;
 
     public PieceView(String name) {
         this.circle = new Circle(5, Color.ORCHID);
-        this.label = createLabel(name);
-        this.getChildren().addAll(circle, label);
+        this.text = createText(name);
+        this.getChildren().addAll(circle, text);
     }
 
-    private Label createLabel(String name) {
-        Label label = new Label(name);
-        label.setWrapText(true);
-        label.setTextAlignment(TextAlignment.CENTER);
-        label.setAlignment(Pos.CENTER);
-        return label;
+    private Text createText(String name) {
+        Text text = new Text(name);
+        text.setTextAlignment(TextAlignment.CENTER);
+        return text;
     }
 
     public void updatePieceViewSize(double tileSize) {
         final double CIRCLE_CONTENT_PERCENTAGE = 0.9;
         final double LABEL_CONTENT_PERCENTAGE = 0.9;
         circle.setRadius((tileSize * CIRCLE_CONTENT_PERCENTAGE) / 2);
-        label.setPrefWidth(tileSize * CIRCLE_CONTENT_PERCENTAGE * LABEL_CONTENT_PERCENTAGE);
+        text.setWrappingWidth(tileSize * CIRCLE_CONTENT_PERCENTAGE * LABEL_CONTENT_PERCENTAGE);
     }
 }

28c78cc86074cde053ca3ea9e65b0cce3ea096c2
rmit-s3629412-QI-SUN <42260597+rmit-s3629412-QI-SUN@users.noreply.github.com>
2020-04-19 01:07:19 +1000

    add Action Button Event into the Controller

--- src/skyvssea/controller/Controller.java
+++ src/skyvssea/controller/Controller.java
@@ -1,5 +1,138 @@
 package skyvssea.controller;
 
+import skyvssea.model.*;
+import skyvssea.view.ActionPane;
+import skyvssea.view.BoardPane;
+
 public class Controller {
 
+    private Board board;
+    private PieceManager pieceManager;
+    private BoardPane boardPane;
+    //
+    private ActionPane actionPane;
+    TurnManager turnManager = new TurnManager();
+    Player[] players;
+    Player currentPlayer;
+
+
+    public Controller() {
+        turnManager.initPlayer();
+        players = turnManager.players;
+        currentPlayer = players[0];
+    }
+
+    public void handleTileClicked(int x, int y) {
+    	
+    	// TODO: use the tileView position to find the Tile object instead
+    	
+    	Tile tile = board.getTile(x, y);
+        if (tile.isHighlighted()) {
+            board.clearHighlightedTiles();
+            tile.setPiece(pieceManager.getCurrentPiece());
+            board.getCurrentTile().removePiece();
+        } else {
+            board.clearHighlightedTiles();
+            if (tile.hasPiece()) {
+                // Nick - TODO: Check whose the piece belongs to
+                Piece piece = tile.getPiece();
+                pieceManager.setCurrentPiece(piece);
+
+                int numMove = piece.getNumMove();
+                int pieceX = tile.getTilePane().getX();
+                int pieceY = tile.getTilePane().getY();
+                Tile[][] tiles = board.getTiles();
+
+                // Nick - TODO: Find a way to modularize the code
+                // Jiang - TODO: and customize available moves based on Piece type
+                // Highlight possible move tiles
+                for (int count = 1; count <= numMove; count++) {
+                    if ((pieceX + count) < BoardPane.NUM_SIDE_CELL) {
+                        Tile rightTile = tiles[pieceX + count][pieceY];
+                        board.highlightUnoccupiedTiles(rightTile);
+                    }
+
+                    if ((pieceX - count) >= 0) {
+                        Tile leftTile = tiles[pieceX - count][pieceY];
+                        board.highlightUnoccupiedTiles(leftTile);
+                    }
+
+                    if ((pieceY + count) < BoardPane.NUM_SIDE_CELL) {
+                        Tile downTile = tiles[pieceX][pieceY + count];
+                        board.highlightUnoccupiedTiles(downTile);
+                    }
+
+                    if ((pieceY - count) >= 0) {
+                        Tile upTile = tiles[pieceX][pieceY - count];
+                        board.highlightUnoccupiedTiles(upTile);
+                    }
+                }
+            }
+        }
+        board.setCurrentTile(tile);
+    }
+
+
+
+    public void setViewsAndModels(Board board, PieceManager pieceManager, BoardPane boardPane) {
+        this.board = board;
+        this.pieceManager = pieceManager;
+        this.boardPane = boardPane;
+    }
+
+    public void setAction(ActionPane actionPane) {
+        this.actionPane = actionPane;
+    }
+
+    public void setTurnManager(TurnManager turnManager) {
+        this.turnManager = turnManager;
+        this.players = turnManager.players;
+//        this.currentPlayer = turnManager.players[0];
+    }
+
+    // when click skip button, the player turn will be changed
+    //when click kill and special button, the player turn will be changed
+
+    public void addActionHandler() {
+        turnManager.setUpPlayers();
+        currentPlayer.setStatus(true);
+        actionPane.getPlayerText().setText(currentPlayer.getName());
+        actionPane.getPlayerText().setFill(currentPlayer.getColour());
+
+        actionPane.getSkipBtn().setOnAction(actionEvent -> {
+            skipBtnChangePlayerTurn();
+        });
+
+        actionPane.getKillBtn().setOnAction(actionEvent -> {
+            this.actionPane.setKillStatus(true);
+            if(this.actionPane.isSpecialStatus() == true){
+                skipBtnChangePlayerTurn();
+                this.actionPane.setKillStatus(false);
+                this.actionPane.setSpecialStatus(false);
+            }
+        });
+
+        this.actionPane.getSpecialEffectBtn().setOnAction(actionEvent -> {
+            this.actionPane.setSpecialStatus(true);
+            if(this.actionPane.isKillStatus() == true){
+                skipBtnChangePlayerTurn();
+                this.actionPane.setKillStatus(false);
+                this.actionPane.setSpecialStatus(false);
+            }
+        });
+    }
+
+    private void skipBtnChangePlayerTurn(){
+        if(currentPlayer.getName() == players[0].getName()){
+            currentPlayer = players[1];
+            actionPane.getPlayerText().setText(currentPlayer.getName());
+            actionPane.getPlayerText().setFill(currentPlayer.getColour());
+        }
+
+        else if (currentPlayer.getName() == players[1].getName()){
+            currentPlayer = players[0];
+            actionPane.getPlayerText().setText(currentPlayer.getName());
+            actionPane.getPlayerText().setFill(currentPlayer.getColour());
+        }
+    }
 }

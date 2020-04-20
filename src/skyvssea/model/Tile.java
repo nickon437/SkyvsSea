ce9c5f9a8eaf4f4ef178f31dd773d9a93525ee1b
jiangng <jiang.ng7@gmail.com>
2020-04-20 22:14:09 +1000

    removed outdated comments in tile.java

--- src/skyvssea/model/Tile.java
+++ src/skyvssea/model/Tile.java
@@ -2,27 +2,24 @@ package skyvssea.model;
 
 import java.util.Observable;
 
-import skyvssea.view.PieceView;
+import com.google.java.contract.Ensures;
+import com.google.java.contract.Requires;
+
 import skyvssea.view.TilePane;
 
 public class Tile  extends Observable {
-//    private TilePane tileView; //Probably not appropriate to have a View object here in Model; should try using an interface instead such as Listener
 	private int x;
 	private int y;
     private Piece piece;
     private boolean light;
     private boolean isHighlighted;
 
-	public Tile(int x, int y, /* TilePane tilePane, */boolean light) {
-//        this.tileView = tilePane;
+	public Tile(int x, int y, boolean light) {
 		this.x = x;
 		this.y = y;
         this.light = light;
         piece = null;
-//        setHighlighted(false);
     }
-
-//    public TilePane getTilePane() { return tileView; }
 
     public boolean hasPiece() {
         return piece != null;
@@ -32,26 +29,21 @@ public class Tile  extends Observable {
         return piece;
     }
 
-    public boolean setPiece(Piece piece) {
+    public void setPiece(Piece piece) {
         this.piece = piece;
         setChanged();
         notifyObservers(piece.getName());
-        return true;
-//        PieceView pieceView = piece.getPieceView();
-//        return tileView.getChildren().add(pieceView); //TODO: should be handled by observer pattern
     }
 
+    @Ensures("piece == null")
     public void removePiece() {
         piece = null;
-//        setChanged();
-//        notifyObservers("REMOVE_PIECEVIEW");
     }
 
     public boolean isHighlighted() { return isHighlighted; }
 
     public void setHighlighted(boolean isHighlighted) {
         this.isHighlighted = isHighlighted;
-        //TODO: the color changing should be handled by TilePane class and triggered by the observer pattern
         String baseColor;
         if (isHighlighted) {
             baseColor = TilePane.HIGHLIGHED_COLOR;
@@ -62,7 +54,7 @@ public class Tile  extends Observable {
                 baseColor = TilePane.DEFAULT_DARK_BASE_COLOR;
             }
         }
-//        tileView.updateBaseColor(baseColor);
+
         setChanged();
         notifyObservers(baseColor);
     }

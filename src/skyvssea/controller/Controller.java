a3ed963fae6ac99daf5c2e09f33ed77e8c01ad79
rmit-s3762629-ton-binh-on <48907239+rmit-s3762629-ton-binh-on@users.noreply.github.com>
2020-04-21 23:54:01 +1000

    Broke down highlightPossibleMove

--- src/skyvssea/controller/Controller.java
+++ src/skyvssea/controller/Controller.java
@@ -40,40 +40,43 @@ public class Controller {
                 Piece piece = selectedTile.getPiece();
                 if (playerManager.checkSide(piece).equals(playerManager.getCurrentPlayer())) {
                     pieceManager.setCurrentPiece(piece);
-
-                    int numMove = piece.getNumMove();
-                    int pieceX = selectedTile.getX();
-                    int pieceY = selectedTile.getY();
-                    Tile[][] tiles = board.getTiles();
-
-                    // Nick - TODO: Find a way to modularize the code
-                    // Jiang - TODO: and customize available moves based on Piece type
-                    // Highlight possible move tiles
-                    for (int count = 1; count <= numMove; count++) {
-                        if ((pieceX + count) < BoardPane.NUM_SIDE_CELL) {
-                            Tile rightTile = tiles[pieceX + count][pieceY];
-                            board.highlightUnoccupiedTiles(rightTile);
-                        }
-
-                        if ((pieceX - count) >= 0) {
-                            Tile leftTile = tiles[pieceX - count][pieceY];
-                            board.highlightUnoccupiedTiles(leftTile);
-                        }
-
-                        if ((pieceY + count) < BoardPane.NUM_SIDE_CELL) {
-                            Tile downTile = tiles[pieceX][pieceY + count];
-                            board.highlightUnoccupiedTiles(downTile);
-                        }
-
-                        if ((pieceY - count) >= 0) {
-                            Tile upTile = tiles[pieceX][pieceY - count];
-                            board.highlightUnoccupiedTiles(upTile);
-                        }
-                    }
+                    highlightPossibleMoveTiles(piece, selectedTile);
                 }
             }
         }
         board.setCurrentTile(selectedTile);
+    }
+
+    private void highlightPossibleMoveTiles(Piece piece, Tile selectedTile) {
+        int numMove = piece.getNumMove();
+        int pieceX = selectedTile.getX();
+        int pieceY = selectedTile.getY();
+        Tile[][] tiles = board.getTiles();
+
+        // Nick - TODO: Find a way to modularize the code
+        // Jiang - TODO: and customize available moves based on Piece type
+        // Highlight possible move tiles
+        for (int count = 1; count <= numMove; count++) {
+            if ((pieceX + count) < BoardPane.NUM_SIDE_CELL) {
+                Tile rightTile = tiles[pieceX + count][pieceY];
+                board.highlightUnoccupiedTiles(rightTile);
+            }
+
+            if ((pieceX - count) >= 0) {
+                Tile leftTile = tiles[pieceX - count][pieceY];
+                board.highlightUnoccupiedTiles(leftTile);
+            }
+
+            if ((pieceY + count) < BoardPane.NUM_SIDE_CELL) {
+                Tile downTile = tiles[pieceX][pieceY + count];
+                board.highlightUnoccupiedTiles(downTile);
+            }
+
+            if ((pieceY - count) >= 0) {
+                Tile upTile = tiles[pieceX][pieceY - count];
+                board.highlightUnoccupiedTiles(upTile);
+            }
+        }
     }
 
     public void handleSkipButton() {

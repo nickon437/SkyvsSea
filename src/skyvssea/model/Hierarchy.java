b019bc991c47deefe7d35c2f7e0f0608b4524c0f
jiangng <jiang.ng7@gmail.com>
2020-05-02 23:37:22 +1000

    Added values to Hierarchy enum

--- src/skyvssea/model/Hierarchy.java
+++ src/skyvssea/model/Hierarchy.java
@@ -1,8 +1,24 @@
 package skyvssea.model;
 
 public enum Hierarchy {
-	BABY,
-    SMALL,
-    MEDIUM,
-    BIG
+	BABY(0),
+    SMALL(1),
+    MEDIUM(2),
+    BIG(3),
+    STEROID(4);
+	
+	public final int magnitude;
+	
+	private Hierarchy(int magnitude) {
+		this.magnitude = magnitude;
+	}
+	
+	public static Hierarchy valueOfMagnitude(int magnitude) {
+	    for (Hierarchy lvl : values()) {
+	        if (lvl.magnitude == magnitude) {
+	            return lvl;
+	        }
+	    }
+	    return null;
+	}
 }
